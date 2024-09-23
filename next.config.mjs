2c14bba4799b0e3c33d37fde9ce946f2d0f17b3d
Gregory Harris <63122765+gregoryharris869@users.noreply.github.com>
2024-09-23 12:45:10 -0500

    images fixed

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: [
+      "m.media-amazon.com",
+      "static.wikia.nocookie.net",
+      "flxt.tmsimg.com",
+      "i.pinimg.com",
+    ],
+  },
+};
 
 export default nextConfig;
